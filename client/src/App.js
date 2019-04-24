c318c2a28a2a2053308b1e5f393bcf3dd28bbf9b
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-24 16:20:40 -0400

    Now will Deploy Contract via GasStationNetwork

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import getWeb3, { getGanacheWeb3, useRelayer} from "./utils/getWeb3";
+import getWeb3, { getGanacheWeb3, useRelayer } from "./utils/getWeb3";
 import Header from "./components/Header/index.js";
 import Footer from "./components/Footer/index.js";
 import Hero from "./components/Hero/index.js";
 import Instructions from "./components/Instructions/index.js";
-import { Loader, Button } from 'rimble-ui';
+import { Loader, Button } from "rimble-ui";
 
-import { zeppelinSolidityHotLoaderOptions } from '../config/webpack';
+import { zeppelinSolidityHotLoaderOptions } from "../config/webpack";
 
-import styles from './App.module.scss';
+import styles from "./App.module.scss";
 
 class App extends Component {
   state = {
@@ -16,9 +16,9 @@ class App extends Component {
     web3: null,
     accounts: null,
     contract: null,
-    route: window.location.pathname.replace("/",""),
+    route: window.location.pathname.replace("/", ""),
     gaslessNFTName: "none set",
-    totalSupply: "0",
+    totalSupply: "0"
   };
 
   getGanacheAddresses = async () => {
@@ -29,24 +29,20 @@ class App extends Component {
       return await this.ganacheProvider.eth.getAccounts();
     }
     return [];
-  }
+  };
 
   componentDidMount = async () => {
     const hotLoaderDisabled = zeppelinSolidityHotLoaderOptions.disabled;
-    let GaslessCounter = {};
+
     let GaslessNFT = {};
-    let Counter = {};
-    let Wallet = {};
+
     try {
-      GaslessCounter = require("./contracts/GaslessCounter.json");
       GaslessNFT = require("./contracts/MetaNFT.json");
-      Counter = require("./contracts/Counter.json");
-      Wallet = require("./contracts/Wallet.json");
     } catch (e) {
       console.log(e);
     }
     try {
-      const isProd = process.env.NODE_ENV === 'production';
+      const isProd = process.env.NODE_ENV === "production";
       if (!isProd) {
         // Get network provider and web3 instance.
         const web3 = await getWeb3();
@@ -54,7 +50,7 @@ class App extends Component {
         try {
           ganacheAccounts = await this.getGanacheAddresses();
         } catch (e) {
-          console.log('Ganache is not running');
+          console.log("Ganache is not running");
         }
         // Use web3 to get the user's accounts.
         const accounts = await web3.eth.getAccounts();
@@ -62,20 +58,21 @@ class App extends Component {
         const networkId = await web3.eth.net.getId();
         const networkType = await web3.eth.net.getNetworkType();
         const isMetaMask = web3.currentProvider.isMetaMask;
-        let balance = accounts.length > 0 ? await web3.eth.getBalance(accounts[0]): web3.utils.toWei('0');
-        balance = web3.utils.fromWei(balance, 'ether');
-        let gaslessInstance = null;
+        let balance =
+          accounts.length > 0
+            ? await web3.eth.getBalance(accounts[0])
+            : web3.utils.toWei("0");
+        balance = web3.utils.fromWei(balance, "ether");
         let gaslessNFTInstance = null;
-        let instance = null;
-        let instanceWallet = null;
         let deployedNetwork = null;
 
         if (GaslessNFT.networks) {
+          console.log("Deployed networks for NFT", GaslessNFT.networks)
           deployedNetwork = GaslessNFT.networks[networkId.toString()];
           if (deployedNetwork) {
             gaslessNFTInstance = new web3.eth.Contract(
               GaslessNFT.abi,
-              deployedNetwork && deployedNetwork.address,
+              deployedNetwork && deployedNetwork.address
             );
           }
         }
@@ -83,22 +80,42 @@ class App extends Component {
         if (gaslessNFTInstance) {
           // Set web3, accounts, and contract to the state, and then proceed with an
           // example of interacting with the contract's methods.
-          this.setState({ web3, ganacheAccounts, accounts, balance, networkId, networkType, hotLoaderDisabled,
-            isMetaMask, gaslessNFT: gaslessNFTInstance}, () => {
+          this.setState(
+            {
+              web3,
+              ganacheAccounts,
+              accounts,
+              balance,
+              networkId,
+              networkType,
+              hotLoaderDisabled,
+              isMetaMask,
+              gaslessNFT: gaslessNFTInstance
+            },
+            () => {
               this.refreshValues(gaslessNFTInstance);
               setInterval(() => {
                 this.refreshValues(gaslessNFTInstance);
-              }, 5000);
-            });
-        }
-        else {
-          this.setState({ web3, ganacheAccounts, accounts, balance, networkId, networkType, hotLoaderDisabled, isMetaMask });
+              }, 2500);
+            }
+          );
+        } else {
+          this.setState({
+            web3,
+            ganacheAccounts,
+            accounts,
+            balance,
+            networkId,
+            networkType,
+            hotLoaderDisabled,
+            isMetaMask
+          });
         }
       }
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
@@ -110,105 +127,60 @@ class App extends Component {
     }
   }
 
-  refreshValues = (gaslessNFTInstance) => {
-    if(gaslessNFTInstance){
+  refreshValues = gaslessNFTInstance => {
+    if (gaslessNFTInstance) {
       this.getGaslessNFTName();
       this.getTotalNFTSupply();
     }
-  }
+  };
 
   getGaslessNFTName = async () => {
-    const {gaslessNFT} = this.state;
+    const { gaslessNFT } = this.state;
     //Get the Name to prove it's loaded
     const response = await gaslessNFT.methods.name().call();
     console.log("Contract NFT name is: ", response);
-    this.setState({gaslessNFTName: response});
-  }
-
-  updateTokenOwner = async () => {
-    const { wallet, accounts } = this.state;
-    // Get the value from the contract to prove it worked.
-    const response = await wallet.methods.owner().call();
-    // Update state with the result.
-    this.setState({ tokenOwner: response.toString() === accounts[0].toString() });
-  };
-
-  increaseCount = async (number) => {
-    const { accounts, contract } = this.state;
-    await contract.methods.increaseCounter(number).send({ from: accounts[0] });
-    this.getCount();
-  };
-
-  decreaseCount = async (number) => {
-    const { accounts, contract } = this.state;
-    await contract.methods.decreaseCounter(number).send({ from: accounts[0] });
-    this.getCount();
-  };
-
-  increaseGaslessCount = async (number) => {
-    const { accounts, gaslessContract } = this.state;
-    try {
-      await gaslessContract.methods.increaseCounter(number).send({ from: accounts[0] });
-    } catch (err) {
-      window.alert(err);
-    }
-    this.getGaslessCount();
+    this.setState({ gaslessNFTName: response });
   };
 
   initializeGasLessNFT = async () => {
-    const {accounts, gaslessNFT } = this.state;
+    const { accounts, gaslessNFT } = this.state;
     try {
-      await gaslessNFT.methods.initialize("Dennison Token", "DT", [accounts[0]], [accounts[0]]).send({from: accounts[0], gas: 5000000});
+      await gaslessNFT.methods
+        .initialize("Dennison Token", "DT", [accounts[0]], [accounts[0]])
+        .send({ from: accounts[0], gas: 5000000 });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   mintGaslessNFT = async () => {
-    const {accounts, gaslessNFT, totalSupply} = this.state;
+    const { accounts, gaslessNFT, totalSupply } = this.state;
     try {
-      const tx = await gaslessNFT.methods.mintWithTokenURI(accounts[0], totalSupply+1, "first Token1").send({from: accounts[0], gas: 5000000});
+      const tx = await gaslessNFT.methods
+        .mintWithTokenURI(accounts[0], totalSupply + 1, "first Token1")
+        .send({ from: accounts[0], gas: 5000000 });
       console.log(tx);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   getTotalNFTSupply = async () => {
-    const {gaslessNFT} = this.state;
+    const { gaslessNFT } = this.state;
     try {
       const response = await gaslessNFT.methods.totalSupply().call();
       this.setState({ totalSupply: response.toString() });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
-
-
-  decreaseGaslessCount = async (number) => {
-    const { accounts, gaslessContract } = this.state;
-    try {
-      await gaslessContract.methods.decreaseCounter(number).send({ from: accounts[0] });
-    } catch(err) {
-      window.alert(err);
-    }
-    this.getGaslessCount();
-  };
-
-  renounceOwnership = async (number) => {
-    const { accounts, wallet } = this.state;
-    await wallet.methods.renounceOwnership().send({ from: accounts[0] });
-    this.updateTokenOwner();
   };
 
   useRelayerBtnPress() {
-    var btn = document.getElementById('useRelayerBtn');
+    var btn = document.getElementById("useRelayerBtn");
     btn.disabled = true;
-    btn.innerText = 'Using Relayer';
+    btn.innerText = "Using Relayer";
     useRelayer(this.state.web3);
   }
-
 
   renderLoader() {
     return (
@@ -224,15 +196,19 @@ class App extends Component {
     return (
       <div className={styles.setup}>
         <div className={styles.notice}>
-          Your <b> contracts are not deployed</b> in this network. Two potential reasons: <br />
+          Your <b> contracts are not deployed</b> in this network. Two potential
+          reasons: <br />
           <p>
-            Maybe you are in the wrong network? Point Metamask to localhost.<br />
+            Maybe you are in the wrong network? Point Metamask to localhost.
+            <br />
             You contract is not deployed. Follow the instructions below.
           </p>
         </div>
         <Instructions
           ganacheAccounts={this.state.ganacheAccounts}
-          name={instructionsKey} accounts={this.state.accounts}/>
+          name={instructionsKey}
+          accounts={this.state.accounts}
+        />
       </div>
     );
   }
@@ -241,28 +217,36 @@ class App extends Component {
     return (
       <div className={styles.wrapper}>
         {!this.state.web3 && this.renderLoader()}
-        {this.state.web3 && !this.state.gaslessContract && (
-          this.renderDeployCheck('gasless-counter')
-        )}
-        {this.state.web3 && this.state.gaslessContract && (
+        {/* {this.state.web3 &&
+          !this.state.gaslessContract &&
+          this.renderDeployCheck("gasless-counter")} */}
+        {this.state.web3 && this.state.gaslessNFT && (
           <div className={styles.contracts}>
             <h1>Gasless NFT Contract</h1>
-            <p>In order to make gasless transactions, press the 'Use Relayer' button below. </p>
+            <p>
+              In order to make gasless transactions, press the 'Use Relayer'
+              button below.{" "}
+            </p>
             <p>(to stop using relayer simply refresh the page)</p>
-            <Button id='useRelayerBtn' onClick={() => this.useRelayerBtnPress()}>
+            <Button
+              id="useRelayerBtn"
+              onClick={() => this.useRelayerBtnPress()}
+            >
               Use Relayer
             </Button>
-            <Button id='useDeployNFT' onClick={() => this.initializeGasLessNFT()}>
+            <Button
+              id="useDeployNFT"
+              onClick={() => this.initializeGasLessNFT()}
+            >
               Initialize Contract
             </Button>
-            <Button id='mintNFT' onClick={() => this.mintGaslessNFT()}>
+            <Button id="mintNFT" onClick={() => this.mintGaslessNFT()}>
               Mint!
             </Button>
-            The name of your NFT is: {this.state.gaslessNFTName}<br/>
+            The name of your NFT is: {this.state.gaslessNFTName}
+            <br />
             The total supply of your NFT is: {this.state.totalSupply}
-            <div className={styles.widgets}>
-            </div>
-
+            <div className={styles.widgets} />
           </div>
         )}
       </div>
@@ -275,22 +259,21 @@ class App extends Component {
         <Hero />
         <Instructions
           ganacheAccounts={this.state.ganacheAccounts}
-          name="setup" accounts={this.state.accounts} />
+          name="setup"
+          accounts={this.state.accounts}
+        />
       </div>
     );
   }
-
-
-
 
   render() {
     return (
       <div className={styles.App}>
         <Header />
-          {this.state.route === '' && this.renderInstructions()}
-          {this.state.route === 'nft' && this.renderGaslessNFTBody()}
-          {this.state.route === 'counter' && this.renderBody()}
-          {this.state.route === 'gasless-counter' && this.renderGaslessBody()}
+        {this.state.route === "" && this.renderInstructions()}
+        {this.state.route === "nft" && this.renderGaslessNFTBody()}
+        {this.state.route === "counter" && this.renderBody()}
+        {this.state.route === "gasless-counter" && this.renderGaslessBody()}
 
         <Footer />
       </div>
